8a45ed129c7a4e9c1d791eab4b3893d7dad5b883
Edmore <3079295@myuwc.ac.za>
2018-11-13 12:36:51 +0200

    auth0 exercise

---
+++ Auth0/validation.js
@@ -0,0 +1,35 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        usname: '',
+        pswd: '',
+        usernameError: false,
+        passwordError: false,
+        usernameErrorMessage: [],
+        passwordErrorMessage: ''
+    },
+    methods: {
+        showValidation: function (){
+                this.usernameError = false
+                this.passwordError = false
+                this.usernameErrorMessage = []
+
+            if(this.pswd.length < 6){
+                   
+            }
+            
+            if(this.usname <= 3){
+                this.usernameError = true
+               this.usernameErrorMessage.push({msg: 'Username is too short', date: Date.now()})
+               var passwordErrorMessage = {
+                   msg: 'new Error Message',
+                   date: Date.now()
+               }
+            }
+            if(!this.usname.includes('@')){
+                this.usernameError = true
+                this.usernameErrorMessage.push({msg: 'Username must be a valid email address', date: Date.now()}) 
+            }
+        }
+    }
+})
